0510121df78e30e75abad855db4dcd1c3de5df29
LilianRaquelQuimbitaQuimbita <USRDEL@USR3.local>
2017-05-03 13:40:01 -0500

    comandosBasicos-Javascript

---
+++ 02-javascript/Variables/variables.js
@@ -0,0 +1,41 @@
+
+//variables.js
+//en java scripot se define una variable con la palabra reservada
+//<var>
+//ENTEROS
+
+var dos=2;
+var tres=3;
+var quince=15;
+var conexionDeBaseDeDatos;
+
+console.log(dos);
+
+//FLOAT
+// no olvidarse simpre la primera minuscula las demas mayusculas
+var dosPuntoCinco=2.5;
+console.log(dosPuntoCinco);
+
+//todas las demas
+
+var verdaderoOFalso=true;
+var falso =false;
+var nombre='Lilian';
+var negativo= -4;
+var fechaNacimiento= new Date();
+
+// no hace falta el casteo se va igualando segun
+//lo que vayamos necesitando
+
+var cuatroString='4';
+cuatroString=4;
+cuatroString=true;
+cuatroString=false;
+cuatroString=-5432;
+cuatroString=new Date();
+cuatroString=undefined;
+cuatroString=null;
+// variable que no tenga nada
+var variable;
+console.log(variable);
+console.log(cuatroString);
