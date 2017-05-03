f34c2a4c4877066076f36240d06b0fb2fdbb9a7a
LilianRaquelQuimbitaQuimbita <USRDEL@USR3.local>
2017-05-03 14:21:28 -0500

    Objetos-Arrays

--- 02-javascript/Variables/variables.js
+++ 02-javascript/Variables/variables.js
@@ -35,7 +35,86 @@ cuatroString=-5432;
 cuatroString=new Date();
 cuatroString=undefined;
 cuatroString=null;
+
 // variable que no tenga nada
 var variable;
 console.log(variable);
 console.log(cuatroString);
+
+// VARIABLES OBJETOS JSON
+
+var lilian=
+    {
+        id:1,
+        nombre:'Lilian',
+        createdAt:new Date(),
+        updatedAt:new Date(),
+        casado:true,
+        prestamos:false,
+        hijos:null,
+        altura:1.55,
+        negativo:-1,
+
+        //OBJETO DENTRO DE UN OBJETO
+        mascota:{
+                nombre: 'Panchito',
+                color: 'blanco'
+            }
+
+    };
+
+//Concatenar un objeto con String
+// no se usa el + sino la coma
+console.log('OBJETO',lilian);
+
+//acceder a cada uno d elos atributos dentro de nuestro objeto
+console.log('Estado: ',lilian.casado);
+// para setear con un nuevo valor el atributo
+lilian.casado=false;
+console.log('Estado: ',lilian.casado);
+
+//acceder atributos de la mascota
+console.log('Mascota: ',lilian.mascota.color);
+
+//borrar un atributo de un objeto
+delete lilian.altura;
+//Añadiendo atrobutos a un objeto
+lilian.juegos='varios';
+
+//DEFINIR un arreglo con todas las variables
+//guardar objetos dentro de un arreglo
+var arreglo=
+    [
+        1,'Raquel',24.5, false, true, null,undefined, new Date(),
+        {
+            a:'b'
+        }
+    ];
+console.log(arreglo);
+//imprimir una posicion
+console.log(arreglo[4]);
+
+// guardar un arreglo dentro de un objeto
+
+var nuevoObjeto={
+    papas:['chola','paiteña']
+};
+ console.log('Papas',nuevoObjeto);
+// Matriz
+var matriz =[[0,1],[2,3]];
+console.log(matriz[0][0]);
+
+var arregloDeObjetos=[
+    {
+        id:1,
+        nombre:'jose'
+    },
+    {
+        id:2,
+        nombre:'felipe'
+    }
+
+];
+
+console.log('Nombres',arregloDeObjetos);
+
