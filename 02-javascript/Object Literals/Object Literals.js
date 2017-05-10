9d671a1ee0864cb725fd89a5ceea27835fc4e801
LilianRaquelQuimbitaQuimbita <USRDEL@USR3.local>
2017-05-10 13:38:17 -0500

    Object Literals

---
+++ 02-javascript/Object Literals/Object Literals.js
@@ -0,0 +1,19 @@
+
+//clic derecho inspeccionar
+var nuevaFuncion =(nombre,apellido)=>{
+
+    var usuario1={
+        nombre:nombre,
+        apellido
+    };
+
+    //alert(' Hola '+usuario1.nombre+usuario1.apellido);
+    console.log(' Hola '+usuario1.nombre+usuario1.apellido);
+    var usuario2={
+        nombre
+    };
+    console.log(usuario2.nombre);
+}
+nuevaFuncion('Lilian',' Quimbita')
+
+ 
