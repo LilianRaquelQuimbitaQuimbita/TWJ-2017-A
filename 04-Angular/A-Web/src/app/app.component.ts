74a55c42c820587b8d124f8168dec8f6761b0d82
LilianRaquelQuimbitaQuimbita <USRDEL@USR3.local>
2017-05-17 15:04:33 -0500

    Angular-Imagenes en html

--- 04-Angular/A-Web/src/app/app.component.ts
+++ 04-Angular/A-Web/src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'twj-raiz',
   templateUrl: './app.component.html',
+  //  template:"Hola Mundo",
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Funciona!!!';
-  nombre:string="Lilian";
-  apellido:string="Quimbita";
+  usuario:UsuarioInt = {
+    nombre:"Lilian",
+    apellido:"Quimbita"
+  }
+
+  constructor(){
+    console.log("Constructor");
+    this.holaMundo();
+    console.log(
+      this.saludar(this.usuario.nombre,
+        this.usuario.apellido
+      ));
+  }
+
+  holaMundo(){
+    console.log("Hola mundo");
+  }
+  saludar(nombre:string,
+          apellido?:string):string{
+    return `Hola ${nombre} ${apellido}`;
+  }
+
+}
+interface UsuarioInt{
+  nombre:string,
+  apellido:string
 }
